78b6fbff0c4545b4b361b6753c31c9f75ffe38ef
Gerson Altamirano <gersonaltamirano@gmail.com>
2019-05-17 11:12:55 -0600

    creando página vue de prueba

---
+++ nuxt/pages/prueba.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>aca van las pruebas</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "prueba"
+    }
+</script>
+
+<style scoped>
+
+</style>
