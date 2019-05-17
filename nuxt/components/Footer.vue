0061197e2c187d3ce13466d555e7692c4bab78e3
Gerson Altamirano <gersonaltamirano@gmail.com>
2019-05-17 11:11:38 -0600

    Agregando bandera de guatemala al Footer

--- nuxt/components/Footer.vue
+++ nuxt/components/Footer.vue
@@ -1,6 +1,7 @@
 <template>
   <footer class="app-footer">
     <span><a href="https://solucionesaltamirano.com">Soluciones Altamirano</a> &copy; 2019.</span>
+    <span class="ml-auto flag-icon flag-icon-gt"></span>
     <!--<span class="ml-auto">Powered by <a href="http://coreui.io">CoreUI</a></span>-->
   </footer>
 </template>
