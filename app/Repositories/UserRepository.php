949e5b1f8904e6498aa1d5b7eed4e3d414d80329
Gerson Altamirano <gersonaltamirano@gmail.com>
2019-05-17 01:22:09 -0600

    Inicio del SysBase v3 con Laravel Infyom y NUXT

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+use App\Repositories\BaseRepository;
+
+/**
+ * Class UserRepository
+ * @package App\Repositories
+ * @version May 17, 2019, 7:04 am UTC
+*/
+
+class UserRepository extends BaseRepository
+{
+    /**
+     * @var array
+     */
+    protected $fieldSearchable = [
+        'name',
+        'email',
+        'email_verified_at',
+        'password',
+        'remember_token'
+    ];
+
+    /**
+     * Return searchable fields
+     *
+     * @return array
+     */
+    public function getFieldsSearchable()
+    {
+        return $this->fieldSearchable;
+    }
+
+    /**
+     * Configure the Model
+     **/
+    public function model()
+    {
+        return User::class;
+    }
+}
---
+++ nuxt/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="app-footer">
+    <span><a href="https://solucionesaltamirano.com">Soluciones Altamirano</a> &copy; 2019.</span>
+    <!--<span class="ml-auto">Powered by <a href="http://coreui.io">CoreUI</a></span>-->
+  </footer>
+</template>
+<script>
+export default {
+  name: 'c-footer'
+}
+</script>
