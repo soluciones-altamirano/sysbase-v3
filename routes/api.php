60f78a55eec060efe6cc59cf40bb4cf0c9051e88
Gerson Altamirano <gersonaltamirano@gmail.com>
2019-05-17 19:41:13 -0600

    creando rutas para login, logout y perfil

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,10 @@ use Illuminate\Http\Request;
 
 Route::group(['prefix'=>'/auth', ['middleware'=>'throttle:20,5']], function (){
     Route::post('/register', 'Auth\RegisterController@register');
-    Route::post('/login', 'Auth\LoginController@login');
+    Route::post('/login', 'Auth\Login   Controller@login');
+});
+
+Route::group(['middleware' => 'jwt.auth'], function (){
+    Route::get('/me', 'MeController@index');
+    Route::get('/auth/logout', 'MeController@logout');
 });
