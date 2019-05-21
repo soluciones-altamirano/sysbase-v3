0cdbc45aa8075684f56d3524989e70157a54db8a
Gerson Altamirano <gersonaltamirano@gmail.com>
2019-05-21 00:01:19 -0600

    Layout por default para usuarios guest (no logueados)

---
+++ nuxt/layouts/guest.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app">
+      <AppHeader />
+      <nuxt />
+      <AppFooter class="fixed-bottom p-3"/>
+  </div>
+</template>
+
+<script>
+import nav from './menu'
+import { Header as AppHeader,
+    Footer as AppFooter,
+} from '~/components/'
+
+export default {
+  name: 'full',
+  components: {
+    AppHeader,
+    AppFooter
+  }
+}
+</script>
