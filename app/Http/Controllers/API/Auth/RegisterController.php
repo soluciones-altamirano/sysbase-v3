5a690ef761426c5b916a7a9752fef98c6d75ea2c
Gerson Altamirano <gersonaltamirano@gmail.com>
2019-05-21 00:06:34 -0600

    fix del error a mostrar

--- app/Http/Controllers/API/Auth/RegisterController.php
+++ app/Http/Controllers/API/Auth/RegisterController.php
@@ -68,7 +68,7 @@ class RegisterController extends Controller
         return response()->json([
             'success' => false,
             'errors' => $validator->errors()
-        ],400);
+        ],422);
     }
 
     /**
