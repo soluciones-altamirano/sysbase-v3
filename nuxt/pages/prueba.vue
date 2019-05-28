e60667a48d0a1665fb6613f6ed1347531b68a998
Gerson Altamirano <gersonaltamirano@gmail.com>
2019-05-28 09:33:27 -0600

    prueba

--- nuxt/pages/prueba.vue
+++ nuxt/pages/prueba.vue
@@ -5,6 +5,10 @@
             <b-badge href="#" variant="success">Sucddccess</b-badge>
         </h1>
 
+        <!--<div v-for="user in users" :key="user.id">-->
+            <!--<span v-text="user.name"></span>-->
+        <!--</div>-->
+
         <div class="text-center">
             <b-button variant="primary">
                 Notifications <b-badge variant="light">4</b-badge>
@@ -16,14 +20,31 @@
 <script>
     export default {
         name: "prueba",
-        // fetch ({ store, redirect }) {
-        //     if (!this.$store.state.auth.user) {
-        //         return redirect('/theme/typography')
+        auth: false,
+        // data(){
+        //     return {
+        //         users: []
         //     }
+        // },
+        // created(){
+        //     this.getUsers
+        // },
+        // methods: {
+        //     async getUsers() {
+        //         try {
+        //           await this.$axios.$get('/users')
+        //               .then(res =>{
+        //                   console.log(res)
+        //                   return {
+        //                       users:res.data
+        //                   }
+        //               })
+        //         }catch (e) {
+        //             return;
+        //         }
+        //
+        //     },
+        //
         // }
     }
 </script>
-
-<style scoped>
-
-</style>
