599d618f1834387b26231e581c1c06c80bd75838
Gerson Altamirano <gersonaltamirano@gmail.com>
2019-05-28 09:30:52 -0600

    modificar el header para poder hacer login y logout

--- nuxt/components/Header/Header.vue
+++ nuxt/components/Header/Header.vue
@@ -3,13 +3,18 @@
     <button class="navbar-toggler mobile-sidebar-toggler d-lg-none" type="button" @click="mobileSidebarToggle">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <b-link class="navbar-brand" to="#"></b-link>
+    <b-link class="navbar-brand" to="/"></b-link>
     <button class="navbar-toggler sidebar-toggler d-md-down-none" type="button" @click="sidebarToggle">
       <span class="navbar-toggler-icon"></span>
     </button>
     <b-navbar-nav class="d-md-down-none">
-      <b-nav-item class="px-3">Dashboard</b-nav-item>
-      <b-nav-item class="px-3">Users</b-nav-item>
+      <b-nav class="px-3" >
+          <nuxt-link to="/dashboard" class="text-decoration-none"> Dashboard  </nuxt-link>
+      </b-nav>
+        <b-nav class="px-3" >
+            <nuxt-link to="/user" class="text-decoration-none"> User  </nuxt-link>
+        </b-nav>
+
       <b-nav-item class="px-3">Settings</b-nav-item>
     </b-navbar-nav>
     <b-navbar-nav class="ml-auto">
--- nuxt/components/Header/HeaderDropdown.vue
+++ nuxt/components/Header/HeaderDropdown.vue
@@ -1,21 +1,35 @@
 <template>
       <b-nav-item-dropdown right no-caret>
         <template slot="button-content">
-          <img src="~static/img/avatars/6.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">
+          <img src="~static/img/avatars/1.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">
         </template>
-        <b-dropdown-header tag="div" class="text-center"><strong>Account</strong></b-dropdown-header>
-        <b-dropdown-item><i class="fa fa-bell-o"></i> Updates<b-badge variant="info">{{itemsCount}}</b-badge></b-dropdown-item>
-        <b-dropdown-item><i class="fa fa-envelope-o"></i> Messages<b-badge variant="success">{{itemsCount}}</b-badge></b-dropdown-item>
-        <b-dropdown-item><i class="fa fa-tasks"></i> Tasks<b-badge variant="danger">{{itemsCount}}</b-badge></b-dropdown-item>
-        <b-dropdown-item><i class="fa fa-comments"></i> Comments<b-badge variant="warning">{{itemsCount}}</b-badge></b-dropdown-item>
-        <b-dropdown-header tag="div" class="text-center"><strong>Settings</strong></b-dropdown-header>
-        <b-dropdown-item><i class="fa fa-user"></i> Profile</b-dropdown-item>
-        <b-dropdown-item><i class="fa fa-wrench"></i> Settings</b-dropdown-item>
-        <b-dropdown-item><i class="fa fa-usd"></i> Payments<b-badge variant="secondary">{{itemsCount}}</b-badge></b-dropdown-item>
-        <b-dropdown-item><i class="fa fa-file"></i> Projects<b-badge variant="primary">{{itemsCount}}</b-badge></b-dropdown-item>
-        <b-dropdown-divider></b-dropdown-divider>
-        <b-dropdown-item><i class="fa fa-shield"></i> Lock Account</b-dropdown-item>
-        <b-dropdown-item @click="logout"><i class="fa fa-lock"></i> Logout</b-dropdown-item>
+        <span v-show="authenticated">
+            <b-dropdown-header tag="div" class="text-center"><strong>Cuenta</strong></b-dropdown-header>
+            <b-dropdown-item><i class="fa fa-bell-o"></i> Actualizaciones<b-badge variant="info">{{itemsCount}}</b-badge></b-dropdown-item>
+            <b-dropdown-item><i class="fa fa-envelope-o"></i> Mensajes<b-badge variant="success">{{itemsCount}}</b-badge></b-dropdown-item>
+            <b-dropdown-item><i class="fa fa-tasks"></i> Tareas<b-badge variant="danger">{{itemsCount}}</b-badge></b-dropdown-item>
+            <b-dropdown-item><i class="fa fa-comments"></i> Comentarios<b-badge variant="warning">{{itemsCount}}</b-badge></b-dropdown-item>
+            <b-dropdown-header tag="div" class="text-center"><strong>Configuraciones</strong></b-dropdown-header>
+            <b-dropdown-item><i class="fa fa-user"></i> Perfil</b-dropdown-item>
+            <b-dropdown-item><i class="fa fa-wrench"></i> Configuración</b-dropdown-item>
+            <b-dropdown-item><i class="fa fa-usd"></i> Pagos<b-badge variant="secondary">{{itemsCount}}</b-badge></b-dropdown-item>
+            <b-dropdown-item><i class="fa fa-file"></i> Proyectos<b-badge variant="primary">{{itemsCount}}</b-badge></b-dropdown-item>
+            <b-dropdown-divider></b-dropdown-divider>
+            <b-dropdown-item><i class="fa fa-shield"></i> Eliminar Cuenta</b-dropdown-item>
+            <b-dropdown-item @click.prevent="singOut"><i class="fa fa-lock"></i> Salir</b-dropdown-item>
+        </span>
+          <span v-show="!authenticated">
+              <b-dropdown-header tag="div" class="text-center"><strong>Cuenta</strong></b-dropdown-header>
+
+                  <nuxt-link to="/auth/login" class="text-decoration-none dropdown-item">
+                      <i class="fa fa-user"></i> Ingresar
+                  </nuxt-link>
+
+              <nuxt-link to="/auth/register" class="text-decoration-none dropdown-item">
+                  <i class="fa fa-lock"></i> Registrarse
+              </nuxt-link>
+
+          </span>
       </b-nav-item-dropdown>
 </template>
 
@@ -23,17 +37,13 @@
   export default {
     name: 'header-dropdown',
     data: () => {
-      return { itemsCount: 42 }
+      return { itemsCount: 77 }
     },
     methods: {
-      async logout() {
-        try {
-          this.$store.dispatch('logout').then(() => {
-            this.$router.push('/login')
-          })
-        } catch (e) {
-          this.formError = e.message
-        }
+      singOut(){
+          this.$auth.logout();
+          this.$router.push({ name: 'index' });
+
       }
     }
   }
