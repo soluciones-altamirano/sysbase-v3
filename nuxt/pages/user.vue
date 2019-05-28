86b3d4a4c186c29daedda8b45ed87995ba6e27ec
Gerson Altamirano <gersonaltamirano@gmail.com>
2019-05-28 09:32:19 -0600

    pagina para ver datos del usuario

---
+++ nuxt/pages/user.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>
+            {{ user.name }}
+        </h1>
+        <p>
+            {{ user.username }}
+        </p>
+        <p>
+            {{ user.email }}
+        </p>
+        <data>
+            {{ user.created_at }}
+        </data>
+
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "user",
+        // middleware: 'guest'
+    }
+</script>
+
+<style scoped>
+
+</style>
